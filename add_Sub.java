12796b3c51c77e25187110c032c579d156374db7
DasariDinesh <dineshdasari05@gmail.com>
2025-08-06 10:35:40 +0530

    Add files via upload

---
+++ add_Sub.java
@@ -0,0 +1,15 @@
+public class add_Sub {
+	public static void main (String args[]) {
+int A=12;
+int B=6;
+int Sum=A+B;
+int Sub=A-B;
+int Mul=A*B;
+int Div=A/B;
+System.out.println("Addition: "+Sum);
+System.out.println("Subtraction: "+Sub);
+System.out.println("Multiplication: "+Mul);
+System.out.println("Division: "+Div);
+
+	}
+}
