89c0cb8fdb2686fd9379d2df9cfd8e112fc8b8f4
DasariDinesh <dineshdasari05@gmail.com>
2025-08-06 10:35:17 +0530

    Add files via upload

---
+++ Integers_Greater_Equal.java
@@ -0,0 +1,15 @@
+
+public class Integers_Greater_Equal {
+	public static void main(String arg[]) {
+	    int a=20,b=20;
+	    if(a>b) {
+	      System.out.println(a+" is greter than"+b);
+	    }
+	    else if(a<b){
+	      System.out.println(a+" is less than "+b);    
+	    }
+	    else {
+	      System.out.print(a+"is equal to "+b);
+	    }
+	}
+}
